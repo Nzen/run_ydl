3ee595e4e954e1cd63d3730eab73fb83aee9eb59
Nzen <nmprado@csupomona.edu>
2015-06-13 15:14:27 -0500

    31 May 2015, flags in batch file
    Before the prior commit run_yd just took youtube vid strings. There's a
    step in between this and prior where I had flags for playlist and yt vid.
    At this point, I've given in and have flags so a line can be for the sites
    I typically take from.

--- run_yd.py
+++ run_yd.py
@@ -1,40 +1,58 @@
 import os
-from sys import argv
 
 '''
 youtube-dl is great, but it doesn't seem to grab playlist arguments from a batch file.
-As I don't want a whole playlist at once, this module solves that limitation.
+As I don't want a whole playlist at once, this module accomodates my prefeence.
 
-moving forward, I may just have flags for every video (macro supplied)
- that way, I can intersperse soundcloud etc. Not tonight
+ Future:
+use proper ipc to run ydl and harvest error signals, for retries
 
  - file format -
-\d+(,\d+)* [\d-_\w]+
-
- - cli run -
-python run_yd.py [username@google_expects] \"[password_may_need_escaping]\"
+(&  \d+(,\d+)* | https://www.youtube.com/watch?v= | ) [\d-_\w]+
 
  - out to system -
-youtube-dl -f 18 -o "%(uploader)s %(upload_date)s %(title)s.%(ext)s" --playlist-items 6,7,8 [playlist_url]
+youtube-dl -f 18 -o "%(upload [...] itle)s.%(ext)s" --playlist-items 6,7,8 [playlist_url]
 '''
 
-def main( username ) :
-	file = open( "0.txt" ) # or your preference, argv also possible
+def main() :
+	file = open( "1.txt" ) # or your preference, argv also possible
 	playlists = file.readlines()
 	file.close()
-
+	flags = {
+		'&': ['-f 18 ','yt '],
+		'y' : ['-f 18 ','yt '],
+		'b' : ['','bc '],
+		'd' : ['-f standard ','dm'],
+		'v' : ['-f h264-sd ','vi'],
+		's' : ['','sc ']
+	}
 	for_bash = ""
-	specific = " -i "
-	command = "youtube-dl -f 18 -o \"%(uploader)s %(upload_date)s %(title)s.%(ext)s\" "
-	identity = "-u "+ username +" -p \"bla\" "
-	for th_line in playlists :
-		if ( th_line[0] == "&" ) :
-			specific = "-i --playlist-items " + th_line[ 2: ] # NOTE full flag includes a space
-		else :
-			specific = " -i "+ th_line
-		#for_bash = "echo " + command + identity + specific # 4TESTS
-		for_bash = command + identity + specific
+	specific = ""
+	flag_type = ""
+	command = [
+		"youtube-dl ",
+		"-o \"%(uploader)s ",
+		"%(upload_date)s %(title)s ",
+		".%(ext)s\" --sleep-interval 2 "
+	]
+	exe, format, out_beg, source, o_name, o_end = 0, 0, 1, 1, 2, 3
+	vid_try = 1
+	for whole_line in playlists :
+		th_line = whole_line.split( '\t' )[0] # the rest is a comment
+		v_type = th_line[0]
+		for_bash = command[exe] + flags[v_type][format] + command[out_beg] \
+			+ flags[v_type][source] + command[o_name] \
+			+ str(vid_try) + command[o_end]
+		if ( v_type == "&" ) :
+			for_bash += "--playlist-items "
+		for_bash += th_line[2:] # ditch flag and space
+		#for_bash = "echo " + for_bash # 4TESTS
+		print
 		os.system( for_bash )
+		vid_try += 1
 
-script, username = argv
-main( username )
+main()
+
+
+
+
