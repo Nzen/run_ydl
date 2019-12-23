90d7075ba8ec5771b5edcbe2ad52211d95546f83
Owner <Owner@DESKTOP-C7KKJR4.home>
2019-12-22 23:32:00 -0800

    feat i700101 uses arg config file
    Uses eno file to fill arg map or falls back to hardcoded
    version. Provides example eno that matches the go lang
    version. Moves java specific changelog to its folder.
    Moves pom elements to canonical locations. Adds a fat zip
    assembly with dependencies and license file.

--- java/src/main/java/ws/nzen/runtime/youtube_dl/RunYd.java
+++ java/src/main/java/ws/nzen/runtime/youtube_dl/RunYd.java
@@ -1,19 +1,25 @@
 /** see ../../../../../LICENSE for release details */
 package ws.nzen.runtime.youtube_dl;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import ws.nzen.format.eno.Eno;
+import ws.nzen.format.eno.ListItem;
+import ws.nzen.format.eno.Section;
+import ws.nzen.format.eno.Value;
 
 /**  */
 @Command(
@@ -24,7 +30,9 @@ import picocli.CommandLine.Parameters;
 public class RunYd implements Runnable
 {
 	private static final String cl = "ry.";
-	private Map<String, String> typeToTemplate;
+	private static final String defaultEnoConfigFilename = "yt_config.eno";
+	private Map<String, String> typeToTemplate = null;
+	private Map<String, List<String>> typeToArgs = null;
 
 	@Parameters(
 			arity = "1",
@@ -38,6 +46,12 @@ public class RunYd implements Runnable
 			paramLabel = "sleep",
 			description = "seconds to wait" )
 	private int sleepSeconds = 5;
+
+	@Option(
+			names = { "-c", "--config" },
+			paramLabel = "config",
+			description = "file with settings" )
+	private Path config;
 
 	/** @param args */
 	public static void main( String[] args )
@@ -55,18 +69,56 @@ public class RunYd implements Runnable
 
 	private void fillTemplate()
 	{
-		if ( batch == null )
+		if ( config == null )
 		{
-			typeToTemplate = new HashMap<>();
-			typeToTemplate.put( "&", "%(uploader)s yt %(upload_date)s %(title)s i" ); // youtube playlist
-			typeToTemplate.put( "y", "%(uploader)s yt %(upload_date)s %(title)s.%(ext)s\" -f 18" ); // youtube vid
-			typeToTemplate.put( "b", "bc %(title)s.%(ext)s\"" ); // http://littlev.bandcamp.com/track/illusive-man
-			typeToTemplate.put( "d", "%(uploader)s dm %(upload_date)s %(title)s.%(ext)s\" -f standard" ); // dailymotion
-			typeToTemplate.put( "v", "%(uploader)s vi %(upload_date)s %(title)s.%(ext)s\" -f http-360p" ); // vimeo
-			typeToTemplate.put( "s", "%(uploader)s sc %(upload_date)s %(title)s.%(ext)s\"" ); // soundcloud
-			typeToTemplate.put( "n", "%(uploader)s ng %(id)s %(title)s.%(ext)s\"" ); // newgrounds ; mp3, needn"t -f
+			File defaultEnoConfig = new File( defaultEnoConfigFilename );
+			if ( defaultEnoConfig.exists() )
+			{
+				config = defaultEnoConfig.toPath();
+				fillFromEnoConfig();
+			}
+			else
+			{
+				typeToTemplate = new HashMap<>();
+				typeToTemplate.put( "&", "%(uploader)s yt %(upload_date)s %(title)s i" ); // youtube playlist
+				typeToTemplate.put( "y", "%(uploader)s yt %(upload_date)s %(title)s.%(ext)s\" -f 18" ); // youtube vid
+				typeToTemplate.put( "b", "bc %(title)s.%(ext)s\"" ); // http://littlev.bandcamp.com/track/illusive-man
+				typeToTemplate.put( "d", "%(uploader)s dm %(upload_date)s %(title)s.%(ext)s\" -f standard" ); // dailymotion
+				typeToTemplate.put( "v", "%(uploader)s vi %(upload_date)s %(title)s.%(ext)s\" -f http-360p" ); // vimeo
+				typeToTemplate.put( "s", "%(uploader)s sc %(upload_date)s %(title)s.%(ext)s\"" ); // soundcloud
+				typeToTemplate.put( "n", "%(uploader)s ng %(id)s %(title)s.%(ext)s\"" ); // newgrounds ; mp3, needn"t -f
+			}
 		}
-		// else parse file for type to template
+		else if ( config.getFileName().toString().endsWith( "eno" ) )
+		{
+			fillFromEnoConfig();
+		}
+	}
+
+
+	private void fillFromEnoConfig()
+	{
+		typeToArgs = new TreeMap<String, List<String>>();
+		Section document = null;
+		try
+		{
+			document = new Eno().deserialize( config );
+		}
+		catch ( IOException ie )
+		{
+			throw new RuntimeException( ie ); // whatever, it's a permissive format
+		}
+		Section formats = document.requiredSection( "flags" );
+		for ( Section format : formats.sections() )
+		{
+			List<String> args = new ArrayList<>();
+			for ( ListItem currArg : format.requiredList( "static args" ).items() )
+			{
+				args.add( currArg.requiredStringValue() );
+			}
+			typeToArgs.put( ((Value)format.requiredField( "flag" ))
+					.requiredStringValue(), args );
+		}
 	}
 
 
@@ -112,6 +164,10 @@ public class RunYd implements Runnable
 			// improve template comes from a file, in the future
 			command[ y_dl ] = "youtube-dl.exe -o \"";
 		}
+		String program = "youtube-dl";
+		if ( ! System.getProperty( "os.name" ).equals( "Linux" ) )
+			program += ".exe";
+		List<String> commandComponents = new LinkedList<String>();
 		for ( String linkLine : links )
 		{
 			if ( linkLine.isEmpty() )
@@ -119,7 +175,25 @@ public class RunYd implements Runnable
 			String flag = linkLine.substring( 0, linkLine.indexOf( " " ) );
 			String rest = linkLine.substring( flag.length() +1 ); // NOTE skip the space
 			String[] linkAndComment = rest.split( "\t" );
-			if ( typeToTemplate.containsKey( flag )
+			commandComponents.clear();
+			// commandComponents.subList( 0, 3 ); // Improve avoid trashing heap each time
+			commandComponents.add( program );
+			commandComponents.add( "--sleep-interval" );
+			commandComponents.add( Integer.toString( sleepSeconds ) );
+			if ( typeToArgs != null
+					&& typeToArgs.containsKey( flag )
+					&& ! linkAndComment[ linkInd ].isEmpty() )
+			{
+				List<String> args = typeToArgs.get( flag );
+				for ( String currArg : args )
+					commandComponents.add( currArg );
+				commandComponents.add( linkAndComment[ linkInd ] );
+				if ( ! useYoutubeDlWith(
+						commandComponents, linkAndComment[ linkInd ] ) )
+					return;
+			}
+			else if ( typeToTemplate != null
+					&& typeToTemplate.containsKey( flag )
 					&& ! linkAndComment[ linkInd ].isEmpty() )
 			{
 				// System.out.println( here +"link is "+ linkAndComment[ linkInd ] );
@@ -133,19 +207,11 @@ public class RunYd implements Runnable
 					String instruction = command[ y_dl ] + typeToTemplate.get( flag )
 							+ command[ sleep ] + linkAndComment[ linkInd ];
 					// System.out.println( here +"instruction | "+ instruction );
-					List<String> commandComponents = new LinkedList<String>();
 					// FIX just hardcoding a valid version for the first use case
 					if ( flag.equals( "y" ) )
 					{
-						commandComponents.add( "youtube-dl" );
-						if ( ! System.getProperty( "os.name" ).equals( "Linux" ) )
-						{
-							commandComponents.set( 0, commandComponents.get( 0 ) +".exe" );
-						}
 						commandComponents.add( "-o" );
 						commandComponents.add( "%(uploader)s yt %(upload_date)s %(id)s %(title)s.%(ext)s" );
-						commandComponents.add( "--sleep-interval" );
-						commandComponents.add( Integer.toString( sleepSeconds ) );
 						commandComponents.add( "-f" );
 						commandComponents.add( "18" );
 						commandComponents.add( linkAndComment[ linkInd ] );
@@ -155,29 +221,41 @@ public class RunYd implements Runnable
 						// NOTE launcher separates these, so it does not work
 						commandComponents.add( instruction );
 					}
-					ProcessBuilder yourJar = new ProcessBuilder( commandComponents );
-					yourJar.inheritIO();
-					try
-					{
-						int resultCode = yourJar.start().waitFor();
-						if ( resultCode != 0 )
-						{
-							System.err.println( here +"saving problem link: "
-									+ flag +" "+ linkAndComment[ linkInd ] );
-						}
-					}
-					catch ( IOException ie )
-					{
-						System.err.println( here +"Couldn't launch downloader because "+ ie );
-					}
-					catch ( InterruptedException ie )
-					{
-						System.err.println( here +"Programmatically told to quit via "+ ie );
+					if ( ! useYoutubeDlWith(
+							commandComponents, linkAndComment[ linkInd ] ) )
 						return;
-					}
 				}
 			}
+			// else quit
 		}
+	}
+
+
+	private boolean useYoutubeDlWith(
+			List<String> commandComponents, String url )
+	{
+		final String here = cl +"uydw ";
+		ProcessBuilder yourJar = new ProcessBuilder( commandComponents );
+		yourJar.inheritIO();
+		try
+		{
+			int resultCode = yourJar.start().waitFor();
+			if ( resultCode != 0 )
+			{
+				System.err.println(
+						here +"saving problem link: "+" "+ url );
+			}
+		}
+		catch ( IOException ie )
+		{
+			System.err.println( here +"Couldn't launch downloader because "+ ie );
+		}
+		catch ( InterruptedException ie )
+		{
+			System.err.println( here +"Programmatically told to quit via "+ ie );
+			return false;
+		}
+		return true;
 	}
 
 }
